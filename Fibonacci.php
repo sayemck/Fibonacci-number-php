b39616ad632f3ac30a33a57d0bf6c00b2e471a83
Abu Sayem Bhuiyan <25548061+sayemck@users.noreply.github.com>
2019-01-10 01:33:21 +0600

    Add files via upload

---
+++ Fibonacci.php
@@ -0,0 +1,16 @@
+<?php
+	function printFibonacci($n){
+		$first = 0;
+		$second = 1;
+		echo "Fibonacci Serise \n";
+		echo $first.' '.$second.' ';
+
+		for ($i =2; $i<$n;$i++){
+			$third=$first+$second;
+			echo $third.' ';
+			$first = $second;
+			$second = $third;
+		}
+		}
+		printFibonacci(6);
+?>
